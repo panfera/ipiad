aec165d41fdd74829dddebb90e66af1f4d63a00a
Your Name <you@example.com>
2023-06-14 22:24:04 +0300

    выявление сущностей

--- src/main/java/mgtu/SiteFetcher/Main.java
+++ src/main/java/mgtu/SiteFetcher/Main.java
@@ -1,21 +1,16 @@
 package mgtu.SiteFetcher;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import org.apache.http.HttpHost;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
-import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
 
 public class Main {
     private static Logger log = LogManager.getLogger();
     //private static Thread taskProducer;
     //private static Thread taskConsumer;
     //private static Thread taskES;
+    private static int count_producer = 2;
+    private static int count_consumer = 2;
+    private static int count_es = 1;
     protected static String author = "Эмма Грибова";
 
     public static void main(String[] args) {
@@ -26,24 +21,22 @@ public class Main {
                     "/",
                     "127.0.0.1",
                     5672);
-            for (int i = 0; i < 1; i++) {
+            for (int i = 0; i < count_es; i++) {
                 Thread taskES = new Thread(new ElasticSearch(rabbitCreds));
                 taskES.start();
             }
 
-            for (int i = 0; i < 0; i++) {
+            for (int i = 0; i < count_producer; i++) {
                 Thread taskProducer = new Thread(new TaskProducer(rabbitCreds));
                 taskProducer.start();
             }
-            for (int i = 0; i < 0; i++) {
+            for (int i = 0; i < count_consumer; i++) {
                 Thread taskConsumer = new Thread(new TaskConsumer(rabbitCreds));
                 taskConsumer.start();
             }
 
-            Analyzer a = new Analyzer();
-            a.run_py_script(author);
-
-
+            Analyzer analyzer = new Analyzer();
+            analyzer.run_py_script(author);
 
         } catch (Exception e) {
             log.error(e);
